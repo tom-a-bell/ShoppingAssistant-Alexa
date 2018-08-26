cf67b6189a272064bd6fe1578f5469790949e98c
Tom Bell <tom.bell.code@gmail.com>
2018-08-26 13:30:11 +0100

    Send changed list items to Alexa Skill on Cognito sync event
    Shopping list items that have changed during the sync event are sent in the message payload to the associated Alex Skill.

--- sync/index.js
+++ sync/index.js
@@ -1,42 +1,50 @@
 const axios = require('axios');
 const querystring = require('querystring');
 
-const sendMessageToAlexaSkill = async (records) => {
-  let accessToken;
-  try {
-    const url = 'https://api.amazon.com/auth/O2/token';
-    const data = {
-      client_id: process.env.ALEXA_SKILL_CLIENT_ID,
-      client_secret: process.env.ALEXA_SKILL_CLIENT_SECRET,
-      grant_type: 'client_credentials',
-      scope: 'alexa:skill_messaging',
-    };
+async function getAccessToken() {
+  const url = 'https://api.amazon.com/auth/O2/token';
+  const data = {
+    client_id: process.env.ALEXA_SKILL_CLIENT_ID,
+    client_secret: process.env.ALEXA_SKILL_CLIENT_SECRET,
+    grant_type: 'client_credentials',
+    scope: 'alexa:skill_messaging',
+  };
 
+  try {
     const response = await axios.post(url, querystring.stringify(data));
-    accessToken = response.data.access_token;
+    return response.data.access_token;
   } catch (error) {
     console.error(error);
+    return undefined;
   }
+}
 
+async function sendMessageToAlexa(records, accessToken) {
   if (!accessToken) {
-    console.error('Failed to obtain access token');
+    console.error('Access token not specified');
     return;
   }
 
-  try {
-    const url = `https://api.amazonalexa.com/v1/skillmessages/users/${process.env.ALEXA_SKILL_USER_ID}`;
-    const data = {
-      data: records,
-      expiresAfterSeconds: 60,
-    };
-    const config = {
-      headers: { Authorization: `Bearer ${accessToken}` },
-    };
+  const userId = process.env.ALEXA_SKILL_USER_ID;
+  const url = `https://api.amazonalexa.com/v1/skillmessages/users/${userId}`;
+  const data = {
+    data: records,
+    expiresAfterSeconds: 60,
+  };
+  const config = {
+    headers: { Authorization: `Bearer ${accessToken}` },
+  };
 
+  try {
     await axios.post(url, data, config);
   } catch (error) {
     console.error(error);
   }
+}
+
+const sendMessageToAlexaSkill = async (records) => {
+  const accessToken = await getAccessToken();
+  await sendMessageToAlexa(records, accessToken);
 };
 
 exports.handler = async (event) => {
@@ -52,44 +60,30 @@ exports.handler = async (event) => {
     return event;
   }
 
-  const modifiedEvent = event;
+  const recordHasChanged = ([, record]) => record && record.newValue !== record.oldValue;
 
-  // Modify value for a key
-  if ('SampleKey1' in event.datasetRecords) {
-    modifiedEvent.datasetRecords.SampleKey1.newValue = 'ModifyValue1';
-    modifiedEvent.datasetRecords.SampleKey1.op = 'replace';
-  }
-
-  // Remove a key
-  if ('SampleKey2' in event.datasetRecords) {
-    modifiedEvent.datasetRecords.SampleKey2.op = 'remove';
-  }
-
-  // // Add a key
-  // if (!('SampleKey3' in event.datasetRecords)) {
-  //     modifiedEvent.datasetRecords.SampleKey3 = {
-  //         newValue: 'ModifyValue3',
-  //         op: 'replace',
-  //     };
-  // }
-
-  // const buildRecords = (records, entry) => {
-  //   const [key, record] = entry;
-  //   records[key] = {
-  //     value: record.op === 'replace' ? record.newValue : record.oldValue,
-  //     change: record.op,
-  //   };
-  //   return records;
-  // };
-
-  // const updatedRecords = event.datasetRecords.filter(record => record.newValue !== record.oldValue);
-  // const recordData = Object.entries(updatedRecords).reduce(buildRecords, {});
-
-  const records = {
-    '2a0c6037-6ff7-4b30-96f1-1debea144aa2': event.datasetRecords['2a0c6037-6ff7-4b30-96f1-1debea144aa2'],
+  const changeForRecord = (record) => {
+    if (record.newValue && !record.oldValue) return 'create';
+    if (!record.newValue && record.oldValue) return 'delete';
+    if (record.newValue !== record.oldValue) return 'update';
+    return 'none';
   };
 
-  await sendMessageToAlexaSkill(records);
+  const buildRecord = ([key, record]) => ({
+    [key]: { change: changeForRecord(record), value: record.op === 'replace' ? record.newValue : record.oldValue },
+  });
 
-  return modifiedEvent;
+  const buildRecords = (records, entry) => ({
+    ...records,
+    ...buildRecord(entry),
+  });
+
+  const changedRecords = Object
+    .entries(event.datasetRecords)
+    .filter(recordHasChanged)
+    .reduce(buildRecords, {});
+
+  await sendMessageToAlexaSkill(changedRecords);
+
+  return event;
 };
